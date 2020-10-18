bbc2c5af8eec422e99f42ff57afc2714a5bc6da7
harmon25 <dougwright1@gmail.com>
2020-10-18 12:15:57 -0400

    use github ref to redex

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule RedexDemo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:redex, path: "../redex"},
+      {:redex, github: "harmon25/redex"},
 
     ]
   end
