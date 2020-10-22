d9919bbe7fd20f27caf3548f72bc1c5ddb7efc61
doug.w <doug.wright@controlgap.com>
2020-10-21 20:13:02 -0400

    dont start repo..

--- lib/redex_demo/application.ex
+++ lib/redex_demo/application.ex
@@ -8,7 +8,7 @@ defmodule RedexDemo.Application do
   def start(_type, _args) do
     children = [
       # Start the Ecto repository
-      RedexDemo.Repo,
+     # RedexDemo.Repo,
       # Start the Telemetry supervisor
       RedexDemoWeb.Telemetry,
       # Start the PubSub system
